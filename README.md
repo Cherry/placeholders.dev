94d75947181a5c1d4f33a465f848e4ccf6af5ac0
James Ross <james@jross.me>
2020-02-19 18:46:34 +0000

    feat: add initial site and API
    todo: improve the website landing page with more docs, examples, build process, favicon, etc.
    todo: logo, OG share image, etc.?
    todo: use `wrangler dev` for testing

---
+++ README.md
@@ -0,0 +1,65 @@
+# [placeholders.dev](https://palceholders.dev)
+Generate super-fast placeholder images in 200+ edge locations, powered by Cloudflare Workers
+![350x150 placeholder image](https://images.placeholders.dev/?width=350&amp;height=100)![200x100 placeholder image](https://images.placeholders.dev/?width=200&amp;height=100&amp;bgColor=%23000&amp;textColor=rgba(255,255,255,0.5))![140x100 placeholder image](https://images.placeholders.dev/?width=140&amp;height=100&amp;bgColor=%23313131&amp;textColor=%23dfdfde)![350x100 placeholder image](https://images.placeholders.dev/?width=350&amp;height=100&amp;text=placeholders)
+![1055x100 placeholder image](https://images.placeholders.dev/?width=1055&amp;height=100&amp;text=Hello%20World&amp;bgColor=%23434343&amp;textColor=%23dfdfde)
+
+
+## Info
+
+placeholders.dev generates custom placeholder images on the fly, such as the examples above. All of these images are generated on Cloudflare's Edge, at 200+ locations, ensuring the best possible performance for all of your users. All images are cached for lengthy periods of time.
+
+### Available API Options
+
+- `width`
+	- Width of generated image. Defaults to `300`.
+- `height`
+	- Height of generated image. Defaults to 150.
+- `text`
+	- Text to display on generated image. Defaults to the image dimensions.
+- `fontFamily`
+	- Font to use for the text. Defaults to `sans-serif`.
+- `fontWeight`
+	- Font weight to use for the text. Defaults to `bold`.
+- `dy`
+	- Adjustment applied to the dy attribute of the text element to appear vertically centered. Defaults to 35% of the font size.
+- `bgColor`
+	- Background color of the image. Defaults to `#ddd`
+- `textColor`
+	- Color of the text. For transparency, use an rgba or hsla value. Defaults to `rgba(0,0,0,0.5)`
+
+Example URL: `https://images.placeholders.dev/?width=350&height=100&text=Hello World&bgColor=%23000`
+## Dev
+
+### Prerequisites
+
+#### Wrangler
+`wrangler` is a CLI tool from Cloudflare, designed to push projects to Cloudflare Workers. See [Cloudflare's documentation](https://developers.cloudflare.com/workers/tooling/wrangler/) for more information.
+
+Install it with npm:
+- `npm i @cloudflare/wrangler -g`
+
+After install run `wrangler config` and follow the prompts to add your Wrangler API token. Follow the instructions in [Cloudflare's](https://developers.cloudflare.com/workers/quickstart/#api-token) docs for information on this. You will not need to edit anything in the `wrangler.toml` file - only your own API token, stored externally.
+
+
+### Build
+- `wrangler build`
+
+### Run
+
+- `npm run start:dev` (this will use `cloudworker` to run a server on `localhost:3000` mimicking the API Worker)
+
+or
+
+- `npm run publish` (push to `placeholders-dev` workers.dev)
+
+or
+
+- `npm run publish:staging` (push to `placeholders-staging` workers.dev)
+
+or
+
+- `npm run publish:preview` (push to Cloudflare Workers preview builder)
+
+## Production
+
+- `npm run publish:prod` (publishes to placeholders.dev via Workers)
---
+++ workers-site/index.js
@@ -0,0 +1,103 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'; // eslint-disable-line node/no-missing-import
+import generateSVG from '@cloudfour/simple-svg-placeholder';
+
+const DEBUG = false;
+
+addEventListener('fetch', event => {
+	try{
+		event.respondWith(handleEvent(event));
+	}catch(e){
+		if(DEBUG){
+			return event.respondWith(
+				new Response(e.message || e.toString(), {
+					status: 500
+				})
+			);
+		}
+		event.respondWith(new Response('Internal Error', { status: 500 }));
+	}
+});
+
+const cacheTtl = 60 * 60 * 24 * 90; // 90 days
+
+async function handleEvent(event){
+	const url = new URL(event.request.url);
+	url.searchParams.sort(); // improve cache-hits by sorting search params
+	console.log(url.searchParams.toString());
+
+	const options = {};
+	const cache = caches.default; // Cloudflare edge caching
+	// when publishing to prod, we serve images from an `images` subdomain
+	// when in dev, we serve from `/api`
+	if(url.host === 'images.placeholders.dev' || url.pathname.startsWith('/api')){
+		// do our API work
+		let response = await cache.match(url, {ignoreMethod: true}); // try to find match for this request in the edge cache
+		if(response){
+			// use cache found on Cloudflare edge. Set X-Worker-Cache header for helpful debug
+			const newHdrs = new Headers(response.headers);
+			newHdrs.set('X-Worker-Cache', "true");
+			return new Response(response.body, {
+				status: response.status,
+				statusText: response.statusText,
+				headers: newHdrs
+			});
+		}
+		const imageOptions = {
+			dataUri: false, // always return an unencoded SVG source
+			width: 300,
+			height: 150,
+			fontFamily: 'sans-serif',
+			fontWeight: 'bold',
+			bgColor: '#ddd',
+			textColor: 'rgba(0,0,0,0.5)'
+		};
+
+		// options that can be overwritten
+		for(const key of ['width', 'height', 'text', 'dy', 'fontFamily', 'fontWeight', 'fontSize', 'bgColor', 'textColor']){
+			if(url.searchParams.has(key)){
+				imageOptions[key] = url.searchParams.get(key);
+			}
+		}
+		response = new Response(generateSVG(imageOptions), {
+			headers: {
+				'content-type': 'image/svg+xml; charset=utf-8'
+			}
+		});
+
+		// set cache header on 200 response
+		if(response.status === 200){
+			response.headers.set('Cache-Control', "public, max-age=" + cacheTtl);
+		}else{
+			// only cache other things for 5 minutes (errors, 404s, etc.)
+			response.headers.set('Cache-Control', 'public, max-age=300');
+		}
+
+		event.waitUntil(cache.put(url, response.clone())); // store in cache
+		return response;
+	}
+
+	// else get the assets from KV
+	try{
+		if(DEBUG){
+			// customize caching
+			options.cacheControl = {
+				bypassCache: true
+			};
+		}
+		return await getAssetFromKV(event, options);
+	}catch(e){
+		// if an error is thrown try to serve the asset at 404.html
+		if(!DEBUG){
+			try{
+				const notFoundResponse = await getAssetFromKV(event, {
+					mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req)
+				});
+
+				return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 });
+			}catch(e){}
+		}
+
+		return new Response(e.message || e.toString(), { status: 500 });
+	}
+}
